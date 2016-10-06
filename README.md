33543a290f4b8f72efd7cdcb8bdacc5d8f32e487
Michael <mrl@nottres.com>
2016-10-06 12:06:28 +0800

    Fix ripple-effect and add card

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 看起來像 Material Design 的幾個頁面。
 
+**提示，這個東西是個玩具。**
+
+作者推薦的 Material Design：
+
+- http://materializecss.com/
+- https://github.com/Daemonite/material
+- https://getmdl.io/
+
+## Goals
+
+建造一堆簡單易用還有一定擴展性的頁面模板和樣式。
+
 ## Basic law
 
 - 不支持古代瀏覽器，不使用除圖標字體、normalize 以外的任何前端庫。
@@ -10,3 +22,8 @@
 - 不使用`這是填充文字這是填充文字這是填充文字……`。
 - 不售賣，不盈利。如果你覺得好，你可以 star 一個。
 - 如果你覺得辣雞，你可以幫忙修改，或者鄙視作者一眼隨後離開。
+
+## Misc
+
+谷歌欽定的配色：https://material.google.com/style/color.html
+MDL 設計參考：https://getmdl.io/components/index.html
--- main.js
+++ main.js
@@ -1,65 +1,85 @@
 _ = document.querySelector.bind(document);
 
-_.drawer = function() {
-  drawer = _('#nav-drawer');
-  if (drawer.classList.contains('active')) {
-    drawer.classList.remove('active');
-  } else {
-    drawer.classList.add('active');
-  }
+_.drawerEventHandler = function() {
+    drawer = _('#nav-drawer');
+    if (drawer.classList.contains('active')) {
+        drawer.classList.remove('active');
+    } else {
+        drawer.classList.add('active');
+    }
 }
-_('h1.site-title').addEventListener('click', _.drawer);
-_('p.drawer-title').addEventListener('click', _.drawer);
 
-// ============================================================
+_.drawerInit = function() {
+    div = document.createElement('div');
+    div.classList = ['nav-drawer-mask'];
+    div.addEventListener('click', _.drawerEventHandler);
+    _('#nav').appendChild(div);
+    _('h1.site-title').addEventListener('click', _.drawerEventHandler);
+    _('p.drawer-title').addEventListener('click', _.drawerEventHandler);
+}
 
-phtext = _('#nav-search input').getAttribute('placeholder');
-placeholder = document.createElement('label');
-placeholder.innerHTML = phtext;
-placeholder.classList.add('search-placeholder');
-_('#nav-search').appendChild(placeholder);
-delete phtext, placeholder;
-_('#nav-search input').removeAttribute('placeholder');
-
-_('#nav-search input').addEventListener('focus', function() {
-  _('#nav-search').classList.add('active');
-});
-_('#nav-search input').addEventListener('blur', function() {
-  _('#nav-search').classList.remove('active');
-});
+_.navSearchInit = function() {
+    placeholderText = _('#nav-search input').getAttribute('placeholder');
+    placeholder = document.createElement('label');
+    placeholder.innerHTML = placeholderText;
+    placeholder.classList.add('search-placeholder');
+    _('#nav-search').appendChild(placeholder);
+    _('#nav-search input').removeAttribute('placeholder');
+    _('#nav-search input').addEventListener('focus', function() {
+        _('#nav-search').classList.add('active');
+    });
+    _('#nav-search input').addEventListener('blur', function() {
+        _('#nav-search').classList.remove('active');
+    });
+}
 
 // ============================================================
 
 _.relativePos = function(mouseEventArg, element) {
-  return {x: mouseEventArg.clientX - element.offsetLeft,
-    y: mouseEventArg.clientY - element.offsetTop};
+    return {
+        x: mouseEventArg.clientX - element.offsetLeft,
+        y: mouseEventArg.clientY - element.offsetTop + document.body.scrollTop
+    };
 }
 
 _.ripple = function(mouseEventArg, element) {
-  div = element.querySelector('.ripple-sphere');
-  pos = _.relativePos(mouseEventArg, element);
-  div.style.left = `${pos.x}px`;
-  div.style.top = `${pos.y}px`;
-  div.classList.add('effect');
+    div = element.querySelector('.ripple-sphere');
+    pos = _.relativePos(mouseEventArg, element);
+    div.style.left = `${pos.x}px`;
+    div.style.top = `${pos.y}px`;
+    div.classList.add('effect');
 }
 
 _.rippleBind = function(element) {
-  div = document.createElement('div');
-  div.classList.add('ripple-sphere');
-  element.appendChild(div);
-  element.addEventListener('mousedown', (function(e) {
-    _.ripple(e, this);
-  }).bind(element));
-  div.addEventListener('transitionend', (function(e) {
-    if (e.propertyName == 'opacity') {
-      if (!this.classList.contains('effect-end')) {
-        this.classList.add('effect-end');
-      } else {
-        this.classList = ['ripple-sphere'];
-      }
-    }
-  }).bind(div));
+    div = document.createElement('div');
+    div.classList = ['ripple-sphere'];
+    element.appendChild(div);
+    element.addEventListener('mousedown', (function(e) {
+        _.ripple(e, this);
+    }).bind(element));
+    div.addEventListener('transitionend', (function(e) {
+        if (e.propertyName == 'opacity') {
+            if (!this.classList.contains('effect-end')) {
+                this.classList.add('effect-end');
+            } else {
+                this.classList = ['ripple-sphere'];
+            }
+        }
+    }).bind(div));
 }
 
-_.rippleBind(_('#ripple-test'));
-_.rippleBind(_('#ripple-test2'));
+_.rippleInit = function() {
+    elements = document.querySelectorAll('.ripple-effect');
+    for (i = 0; i < elements.length; i++) {
+        _.rippleBind(elements[i]);
+    }
+}
+
+// ============================================================
+
+_.documentLoad = function() {
+    _.drawerInit();
+    _.navSearchInit();
+    _.rippleInit();
+}
+document.addEventListener('DOMContentLoaded', _.documentLoad);
